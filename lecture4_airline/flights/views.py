785353ef77ce45cebbba5ef973256479a145e096
GC <2845740149@qq.com>
2022-03-13 13:04:41 +0800

    Fix bugs

--- lecture4_airline/flights/views.py
+++ lecture4_airline/flights/views.py
@@ -9,6 +9,7 @@ def index(request):
         "flights": Flight.objects.all()
     })
 
+
 def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
@@ -16,6 +17,7 @@ def flight(request, flight_id):
         "passengers": flight.passengers.all(),
         "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
+
 
 def book(request, flight_id):
     if request.method == "POST":
